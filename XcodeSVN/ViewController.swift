05c18c5e01ced324bd6bb64704280051c7f6d16c
Mitesh <mitesh.imply@gmail.com>
2023-07-06 20:59:21 +0530

    Add print test

--- XcodeSVN/ViewController.swift
+++ XcodeSVN/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("SVN")
     }
 
     override func viewWillAppear(_ animated: Bool) {
